db63a860f2e96e5eb89c8ed1ddc7c71d71f94bdb
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2021-03-19 16:00:24 +0900

    Add files via upload

---
+++ 0319/opertator.py
@@ -0,0 +1,18 @@
+a = 3
+b = ~a
+print("a=",a, "b=",b)
+
+a = 3
+b = 5
+c = a & b
+print("a=",a, "b=",b, "c=",c)
+
+a = 3
+b = 5
+c = a ^ b 
+print("a=",a, "b=",b, "c=",c)
+
+a = 3
+b = 5
+c = a | b 
+print("a=",a, "b=",b, "c=",c)
