6b22a44d81ac4a5a4b06035de54567fd7f566b99
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2021-04-09 15:47:55 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,10 @@
+fstName = ['H', 'A']
+sndName = ["jae","Beom"]
+stdNum = [2, 0, 2, 1, 0 , 6, 0, 1, 9]
+
+print(fstName)
+print(sndName)
+#화면에 H, J, B를 출력하는 프로그램을 작성하시오.
+print(fstName[0])  #H
+                   #J
+                   #B
