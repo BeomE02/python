fb55261fe7bd79870b7451afe7a7254f95984b83
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2025-04-14 15:45:49 +0900

    Update SList.py

--- 자료구조 2025/SList.py
+++ 자료구조 2025/SList.py
@@ -57,50 +57,109 @@ class SList:
     def appendValue(self, Value):
         self.append(Node(Value))
     #-----------------------------------------------
+    # 리스트의 첫 노드(head)를 삭제하며 반환한다. (빈 리스트이면 None 반환)
+    def unshift(self):
+        if self.head is None:
+            return None
+        else:
+           header = self.head
+           self.head = self.head.next
+           header.next = None
+           self.count -= 1
+           return header
+    #-----------------------------------------------
+    # 리스트의 노드 연결 순서를 역순으로 재구성한다. (헤드가 마지막 노드로 변경된다.)
+    def reverse(self):
+        if self.head is None:
+            return None
+        else:
+            previous = None
+            current = self.head
+            while current is not None:
+                next_node = current.next     # 다음 노드 저장
+                current.next = previous      # 현재 노드의 방향을 이전 노드로 바꿈
+                previous = current           # previous를 현재 노드로 이동
+                current = next_node          # current를 다음 노드로 이동
+            self.head = previous             # 마지막 노드가 새로운 head가 됨
+
+    def reverse2(self):
+        revList = SList() # 역순으로 재구성하는게 활용할 빈 리스트 생성.
+
+        # 기존 리스트의 첫 노드를 차례대로 제거하며 revList에 insertFront() 처리한다.
+        while self.head is not None:
+            h = self.unshift() # 기존 리스트의 첫 노드를 제거한다. (h에 저장)
+            revList.insertFront(h) # 새 리스트(revList)의 insertFront(로) 제거한 노드를 추가한다.
+
+        # 역순으로 재구성된 리스트의 정보를 현재 리스트로 옮겨온다.
+        self.head = revList.head
+        self.count = revList.count
+
+    #-----------------------------------------------
+    # 리스트의 정렬 상태를 유지하면서 새 노드를 추가한다.
+    def insertSorted(self, newNode):
+        if self.head is None:
+            self.head = newNode
+            self.count = 1
+        elif newNode.data < self.head.data:
+            # head보다 작으면 맨 앞에 삽입
+            newNode.next = self.head
+            self.head = newNode
+            self.count += 1
+        else:
+            current = self.head
+            # newNode가 들어갈 위치 바로 앞을 찾는다
+            while current.next is not None and current.next.data < newNode.data:
+                current = current.next
+
+            # 중간 또는 마지막 위치에 삽입
+            newNode.next = current.next
+            current.next = newNode
+            self.count += 1
+            
+    #-----------------------------------------------
     # 특정값을 가진 노드를 연결 리스트에서 제거한다.
     # 반환값 : 제거한 노드를 반환, 없으면 None
 
-
-    # def remove(self, value):
-    #     current = self.head
-    #     previous = None
-
-    #     while current is not None:
-    #         if current.data == value:
-    #             if previous is None:
-    #                 # 삭제할 노드가 맨 앞에 있는 경우
-    #                 self.head = current.next
-    #             else:
-    #                 # 삭제할 노드가 중간 또는 끝에 있는 경우
-    #                 previous.next = current.next
-    #             self.count -= 1
-    #             return current  # 삭제한 노드를 반환
-    #         previous = current
-    #         current = current.next
-    #     return None  # 삭제할 노드를 찾지 못한 경우
-
-
-    # def remove(self, value):
-    # # head가 삭제 대상이면 바로 처리
-    #     if self.head is not None and self.head.data == value:
-    #         removed = self.head
-    #         self.head = self.head.next
-    #         self.count -= 1
-    #         return removed
-
-    #     current = self.head
-    #     # current는 삭제 대상의 바로 앞 노드
-    #     while current is not None and current.next is not None:
-    #         if current.next.data == value:
-    #             removed = current.next
-    #             current.next = current.next.next
-    #             self.count -= 1
-    #             return removed
-    #         current = current.next
-
-    #     return None  # 못 찾은 경우
-    
     def remove(self, value):
+        current = self.head
+        previous = None
+
+        while current is not None:
+            if current.data == value:
+                if previous is None:
+                    # 삭제할 노드가 맨 앞에 있는 경우
+                    self.head = current.next
+                else:
+                    # 삭제할 노드가 중간 또는 끝에 있는 경우
+                    previous.next = current.next
+                self.count -= 1
+                return current  # 삭제한 노드를 반환
+            previous = current
+            current = current.next
+        return None  # 삭제할 노드를 찾지 못한 경우
+
+
+    def remove2(self, value):
+    # head가 삭제 대상이면 바로 처리
+        if self.head is not None and self.head.data == value:
+            removed = self.head
+            self.head = self.head.next
+            self.count -= 1
+            return removed
+
+        current = self.head
+        # current는 삭제 대상의 바로 앞 노드
+        while current is not None and current.next is not None:
+            if current.next.data == value:
+                removed = current.next
+                current.next = current.next.next
+                self.count -= 1
+                return removed
+            current = current.next
+
+        return None  # 못 찾은 경우
+    
+    def remove3(self, value):
         targetNode = self.find(value)
         if targetNode is None:
             return None
@@ -136,10 +195,12 @@ class SList:
 #===================================================
 
 lst = SList()   #빈 리스트 하나 생성.
-lst.insertFront(Node(100))
-lst.insertFront(Node(200))
-lst.insertFront(Node(300))
-lst.showList()
-print(lst.remove(200))
+lst.insertSorted(Node(100))
+lst.insertSorted(Node(200))
+lst.insertSorted(Node(300))
+lst.insertSorted(Node(300))
+lst.insertSorted(Node(400))
+lst.insertSorted(Node(100))
+lst.insertSorted(Node(150))
 lst.showList()
 #===================================================
